74943ed1b572ef516ee9dcace5326505cb7e9d30
Guenther, Karsten (RDD-RM) <Karsten.Guenther@marquardt.com>
2024-09-26 15:08:19 +0200

    solution 1.1: simple tests with printf - passing

--- components/calc/src/calc.h
+++ components/calc/src/calc.h
@@ -8,8 +8,8 @@
  *
  * @param a int16_t: First integer
  * @param b int16_t: Second integer
- * @return int16_t: Sum of a and b
+ * @return int32_t: Sum of a and b
  */
-extern int16_t calc_add(int16_t a, int16_t b);
+extern int32_t calc_add(int16_t a, int16_t b);
 
 #endif /* CALC_H */
