ca3e7f30d04682dae5eb31ec378c3ec8ef0cd6d2
Guenther, Karsten (RDD-RM) <Karsten.Guenther@marquardt.com>
2024-10-11 20:35:34 +0200

    lesson 3: testing in an SPL

--- components/a/test/test_a.cc
+++ components/a/test/test_a.cc
@@ -5,31 +5,21 @@ extern "C"
 {
 #include "a.h"
 #include "b.h"
-
-    int16_t FUNC_GETB1_RETURN_VALUE = 0;
-    int16_t FUNC_GETB2_RETURN_VALUE = 0;
-
-    int16_t getB1()
-    {
-        return FUNC_GETB1_RETURN_VALUE;
-    }
-
-    int16_t getB2()
-    {
-        return FUNC_GETB2_RETURN_VALUE;
-    }
 }
+
+#include "mockup_components_a.h"
+
 // Define a struct to hold the parameters for testing component a
 struct ComponentAParameters
 {
-    const char *description;
+    const char* description;
     int16_t funcGetB1ReturnValue;
     int16_t funcGetB2ReturnValue;
     int32_t expectedResult;
 };
 
 // Override the cout operator for TestParam so that it can be printed in the test output
-std::ostream &operator<<(std::ostream &os, const ComponentAParameters &param)
+std::ostream& operator<<(std::ostream& os, const ComponentAParameters& param)
 {
     os << param.description;
     return os;
@@ -45,8 +35,9 @@ TEST_P(CompATest, FunctionA)
     /* Arrange */
     ComponentAParameters param = GetParam();
 
-    FUNC_GETB1_RETURN_VALUE = param.funcGetB1ReturnValue;
-    FUNC_GETB2_RETURN_VALUE = param.funcGetB2ReturnValue;
+    CREATE_MOCK(myMock);
+    ON_CALL(myMock, getB1()).WillByDefault(Return(param.funcGetB1ReturnValue));
+    ON_CALL(myMock, getB2()).WillByDefault(Return(param.funcGetB2ReturnValue));
 
     /* Act */
     int32_t returnValueA = a();
@@ -60,6 +51,6 @@ INSTANTIATE_TEST_SUITE_P(
     CompATests,
     CompATest,
     ::testing::Values(
-        ComponentAParameters{"Test 1", 7, 5, 13},
-        ComponentAParameters{"Test 2", 38, -142, -103},
-        ComponentAParameters{"Test 3", -322, -338, -659}));
+        ComponentAParameters{ "Test 1", 7, 5, 13 },
+        ComponentAParameters{ "Test 2", 38, -142, -103 },
+        ComponentAParameters{ "Test 3", -322, -338, -659 }));
---
+++ variants/default/parts.cmake
@@ -0,0 +1,4 @@
+spl_add_include(${PROJECT_SOURCE_DIR}/components/b/src)
+
+spl_add_component(components/calc)
+spl_add_component(components/a)
