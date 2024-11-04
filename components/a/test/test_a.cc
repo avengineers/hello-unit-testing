5c13195132d43acd213534cb2fc0b7a2dc27763c
Guenther, Karsten (RDD-RM) <Karsten.Guenther@marquardt.com>
2024-11-04 14:32:10 +0100

    solution 3.1: example with external dependencies - passing

--- components/a/test/test_a.cc
+++ components/a/test/test_a.cc
@@ -5,15 +5,56 @@ extern "C"
 {
 #include "a.h"
 #include "b.h"
+
+    int16_t FUNC_GETB1_RETURN_VALUE = 0;
+    int16_t FUNC_GETB2_RETURN_VALUE = 0;
+
+    int16_t getB1()
+    {
+        return FUNC_GETB1_RETURN_VALUE;
+    }
+
+    int16_t getB2()
+    {
+        return FUNC_GETB2_RETURN_VALUE;
+    }
 }
 
-TEST(CompATest, FunctionA)
+TEST(CompATest, FunctionA_1)
 {
     /* Arrange */
+    FUNC_GETB1_RETURN_VALUE = 7;
+    FUNC_GETB2_RETURN_VALUE = 5;
 
     /* Act */
-    a();
+    int32_t returnValueA = a();
 
     /* Assert */
-    EXPECT_TRUE(false)
+    EXPECT_EQ(returnValueA, 13);
+}
+
+TEST(CompATest, FunctionA_2)
+{
+    /* Arrange */
+    FUNC_GETB1_RETURN_VALUE = 38;
+    FUNC_GETB2_RETURN_VALUE = -142;
+
+    /* Act */
+    int32_t returnValueA = a();
+
+    /* Assert */
+    EXPECT_EQ(returnValueA, -103);
+}
+
+TEST(CompATest, FunctionA_3)
+{
+    /* Arrange */
+    FUNC_GETB1_RETURN_VALUE = -322;
+    FUNC_GETB2_RETURN_VALUE = -338;
+
+    /* Act */
+    int32_t returnValueA = a();
+
+    /* Assert */
+    EXPECT_EQ(returnValueA, -659);
 }
