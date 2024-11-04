5e471faa7a928c51f87fb80db549c7d1d7f0f6c8
Guenther, Karsten (RDD-RM) <Karsten.Guenther@marquardt.com>
2024-11-04 15:09:25 +0100

    solution 4.1: testing in an SPL - passing

--- components/a/test/test_a.cc
+++ components/a/test/test_a.cc
@@ -5,26 +5,17 @@ extern "C"
 {
 #include "a.h"
 #include "b.h"
-
-    int16_t FUNC_GETB1_RETURN_VALUE = 0;
-    int16_t FUNC_GETB2_RETURN_VALUE = 0;
-
-    int16_t getB1()
-    {
-        return FUNC_GETB1_RETURN_VALUE;
-    }
-
-    int16_t getB2()
-    {
-        return FUNC_GETB2_RETURN_VALUE;
-    }
 }
+
+#include "mockup_components_a.h"
 
 TEST(CompATest, FunctionA_1)
 {
     /* Arrange */
-    FUNC_GETB1_RETURN_VALUE = 7;
-    FUNC_GETB2_RETURN_VALUE = 5;
+    CREATE_MOCK(myMock);
+    ON_CALL(myMock, getB1()).WillByDefault(Return(7));
+    ON_CALL(myMock, getB2()).WillByDefault(Return(5));
+
 
     /* Act */
     int32_t returnValueA = a();
@@ -36,8 +27,9 @@ TEST(CompATest, FunctionA_1)
 TEST(CompATest, FunctionA_2)
 {
     /* Arrange */
-    FUNC_GETB1_RETURN_VALUE = 38;
-    FUNC_GETB2_RETURN_VALUE = -142;
+    CREATE_MOCK(myMock);
+    ON_CALL(myMock, getB1()).WillByDefault(Return(38));
+    ON_CALL(myMock, getB2()).WillByDefault(Return(-142));
 
     /* Act */
     int32_t returnValueA = a();
@@ -49,8 +41,9 @@ TEST(CompATest, FunctionA_2)
 TEST(CompATest, FunctionA_3)
 {
     /* Arrange */
-    FUNC_GETB1_RETURN_VALUE = -322;
-    FUNC_GETB2_RETURN_VALUE = -338;
+    CREATE_MOCK(myMock);
+    ON_CALL(myMock, getB1()).WillByDefault(Return(-322));
+    ON_CALL(myMock, getB2()).WillByDefault(Return(-338));
 
     /* Act */
     int32_t returnValueA = a();
