a1926de063484d16b7532bc78d58ac0738a19764
Aleksandar Simeonov <aleksandar.simeonov@gmail.com>
2024-11-05 18:51:31 +0100

    lesson 5: test fixtures - passing

--- components/moving_average/test/test_moving_average.cc
+++ components/moving_average/test/test_moving_average.cc
@@ -6,13 +6,80 @@ extern "C"
 #include "moving_average.h"
 }
 
-TEST(MovingAverageTest, GetAveragedValue)
+class MovingAverageTest : public Test
+{
+protected:
+    void SetUp(void) override
+    {
+        movingAverageInit();
+    }
+};
+
+TEST_F(MovingAverageTest, Initialization)
 {
     /* Arrange */
 
     /* Act */
-    getAveragedValue();
+    int16_t returnAveragedValue = getAveragedValue();
 
     /* Assert */
-    EXPECT_TRUE(false);
+    EXPECT_EQ(returnAveragedValue, 0);
+}
+
+TEST_F(MovingAverageTest, AddSingleValue)
+{
+    /* Arrange */
+    addNewValue(10);
+
+    /* Act */
+    int16_t returnAveragedValue = getAveragedValue();
+
+    /* Assert */
+    EXPECT_EQ(returnAveragedValue, 10);
+}
+
+TEST_F(MovingAverageTest, AddMultipleValues)
+{
+    /* Arrange */
+    addNewValue(10);
+    addNewValue(20);
+    addNewValue(30);
+
+    /* Act */
+    int16_t returnAveragedValue = getAveragedValue();
+
+    /* Assert */
+    EXPECT_EQ(returnAveragedValue, 20);
+}
+
+TEST_F(MovingAverageTest, FullWindow)
+{
+    /* Arrange */
+    addNewValue(10);
+    addNewValue(20);
+    addNewValue(30);
+    addNewValue(40);
+
+    /* Act */
+    int16_t returnAveragedValue = getAveragedValue();
+
+    /* Assert */
+    EXPECT_EQ(returnAveragedValue, 25);
+}
+
+TEST_F(MovingAverageTest, WindowOverflow)
+{
+    /* Arrange */
+    addNewValue(10);
+    addNewValue(20);
+    addNewValue(30);
+    addNewValue(40);
+    addNewValue(50);
+    addNewValue(80);
+
+    /* Act */
+    int16_t returnAveragedValue = getAveragedValue();
+
+    /* Assert */
+    EXPECT_EQ(returnAveragedValue, 50);
 }
