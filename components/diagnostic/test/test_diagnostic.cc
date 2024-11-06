ae3f2c6c6046b4db3833864dd2e96c20723ec4e4
Aleksandar Simeonov <aleksandar.simeonov@gmail.com>
2024-11-06 10:24:55 +0100

    solution 6: expected call - passing

--- components/diagnostic/test/test_diagnostic.cc
+++ components/diagnostic/test/test_diagnostic.cc
@@ -6,13 +6,88 @@ extern "C"
 #include "diagnostic.h"
 }
 
-TEST(DiagnosticTest, FailingTest)
+#include "mockup_components_diagnostic.h"
+
+class DiagnosticTest : public Test
+{
+protected:
+    void SetUp(void) override
+    {
+        diagnosticInit();
+    }
+};
+
+TEST_F(DiagnosticTest, Initialization)
 {
     /* Arrange */
+
+    /* Act */
+
+    /* Assert */
+    EXPECT_FALSE(isDTCSet(HIGH_BEAM_DTC));
+    EXPECT_FALSE(isDTCSet(LOW_BEAM_DTC));
+}
+
+TEST_F(DiagnosticTest, DebounceOnceBothBeams)
+{
+    /* Arrange */
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, runDiagnosticTest(HIGH_BEAM)).Times(1).WillOnce(Return(TEST_FAILED));
+    EXPECT_CALL(myMock, runDiagnosticTest(LOW_BEAM)).Times(1).WillOnce(Return(TEST_FAILED));
 
     /* Act */
     diagnosticCyclic();
 
     /* Assert */
-    EXPECT_TRUE(false);
+    EXPECT_FALSE(isDTCSet(HIGH_BEAM_DTC));
+    EXPECT_FALSE(isDTCSet(LOW_BEAM_DTC));
+}
+
+TEST_F(DiagnosticTest, DebounceTwiceBothBeams)
+{
+    /* Arrange */
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, runDiagnosticTest(_)).Times(4).WillRepeatedly(Return(TEST_FAILED));
+
+    /* Act */
+    diagnosticCyclic();
+    diagnosticCyclic();
+
+    /* Assert */
+    EXPECT_TRUE(isDTCSet(HIGH_BEAM_DTC));
+    EXPECT_TRUE(isDTCSet(LOW_BEAM_DTC));
+}
+
+TEST_F(DiagnosticTest, DebounceThreeTimesBothBeams)
+{
+    /* Arrange */
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, runDiagnosticTest(HIGH_BEAM)).Times(3).WillRepeatedly(Return(TEST_FAILED));
+    EXPECT_CALL(myMock, runDiagnosticTest(LOW_BEAM)).Times(3).WillRepeatedly(Return(TEST_FAILED));
+
+    /* Act */
+    diagnosticCyclic();
+    diagnosticCyclic();
+    diagnosticCyclic();
+
+    /* Assert */
+    EXPECT_TRUE(isDTCSet(HIGH_BEAM_DTC));
+    EXPECT_TRUE(isDTCSet(LOW_BEAM_DTC));
+}
+
+TEST_F(DiagnosticTest, DebounceWithPass)
+{
+    /* Arrange */
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, runDiagnosticTest(HIGH_BEAM)).Times(3).WillOnce(Return(TEST_PASSED)).WillRepeatedly(Return(TEST_FAILED));
+    EXPECT_CALL(myMock, runDiagnosticTest(LOW_BEAM)).Times(3).WillOnce(Return(TEST_FAILED)).WillOnce(Return(TEST_PASSED)).WillOnce(Return(TEST_FAILED));
+
+    /* Act */
+    diagnosticCyclic();
+    diagnosticCyclic();
+    diagnosticCyclic();
+
+    /* Assert */
+    EXPECT_TRUE(isDTCSet(HIGH_BEAM_DTC));
+    EXPECT_FALSE(isDTCSet(LOW_BEAM_DTC));
 }
