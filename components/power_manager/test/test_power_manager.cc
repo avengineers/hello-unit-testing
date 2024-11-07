2b71cfb789d846eb9719ac28ec7dca0de1ca7e8c
Aleksandar Simeonov <aleksandar.simeonov@gmail.com>
2024-11-07 22:13:16 +0100

    solution 10.2: state machine additional state - passing

--- components/power_manager/test/test_power_manager.cc
+++ components/power_manager/test/test_power_manager.cc
@@ -142,6 +142,103 @@ TEST(PowerManagerTest, IdleLowVoltage)
     EXPECT_EQ(current_state, PREPARE_SHUTDOWN);
 }
 
+TEST(PowerManagerTest, LowPowerWaiting)
+{
+    /* Arrange */
+    PowerManagerInit();
+
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, getNetworkControllerStatus()).WillOnce(Return(NETWORK_INITIALIZED));
+    EXPECT_CALL(myMock, getADCValue(_)).WillRepeatedly(DoAll(SetArgPointee<0>(ADC_MIN_VOLTAGE), Return(ADC_READ_OK)));
+    EXPECT_CALL(myMock, getCommunicationStatus()).WillRepeatedly(Return(NO_COMMUNICATION));
+
+    /* Act */
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+
+    PowerManagerState current_state = getCurrentState();
+
+    /* Assert */
+    EXPECT_EQ(current_state, IDLE);
+}
+
+TEST(PowerManagerTest, EnterLowPower)
+{
+    /* Arrange */
+    PowerManagerInit();
+
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, getNetworkControllerStatus()).WillOnce(Return(NETWORK_INITIALIZED));
+    EXPECT_CALL(myMock, getADCValue(_)).WillRepeatedly(DoAll(SetArgPointee<0>(ADC_MIN_VOLTAGE), Return(ADC_READ_OK)));
+    EXPECT_CALL(myMock, getCommunicationStatus()).WillRepeatedly(Return(NO_COMMUNICATION));
+
+    /* Act */
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+
+    PowerManagerState current_state = getCurrentState();
+
+    /* Assert */
+    EXPECT_EQ(current_state, LOW_POWER);
+}
+
+TEST(PowerManagerTest, LowPowerFullCommunication)
+{
+    /* Arrange */
+    PowerManagerInit();
+
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, getNetworkControllerStatus()).WillOnce(Return(NETWORK_INITIALIZED));
+    EXPECT_CALL(myMock, getADCValue(_)).WillRepeatedly(DoAll(SetArgPointee<0>(ADC_MIN_VOLTAGE), Return(ADC_READ_OK)));
+    EXPECT_CALL(myMock, getCommunicationStatus()).WillRepeatedly(Return(NO_COMMUNICATION));
+
+    /* Act */
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+
+    EXPECT_CALL(myMock, getCommunicationStatus()).WillRepeatedly(Return(FULL_COMMUNICATION));
+    powerManagerCyclic();
+
+    PowerManagerState current_state = getCurrentState();
+
+    /* Assert */
+    EXPECT_EQ(current_state, ACTIVE);
+}
+
+TEST(PowerManagerTest, LowPowerShutdown)
+{
+    /* Arrange */
+    PowerManagerInit();
+
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, getNetworkControllerStatus()).WillOnce(Return(NETWORK_INITIALIZED));
+    EXPECT_CALL(myMock, getADCValue(_)).WillRepeatedly(DoAll(SetArgPointee<0>(ADC_MIN_VOLTAGE), Return(ADC_READ_OK)));
+    EXPECT_CALL(myMock, getCommunicationStatus()).WillRepeatedly(Return(NO_COMMUNICATION));
+
+    /* Act */
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+    powerManagerCyclic();
+
+    shutDown();
+    powerManagerCyclic();
+
+    PowerManagerState current_state = getCurrentState();
+
+    /* Assert */
+    EXPECT_EQ(current_state, PREPARE_SHUTDOWN);
+}
+
 TEST(PowerManagerTest, ADCError)
 {
     /* Arrange */
