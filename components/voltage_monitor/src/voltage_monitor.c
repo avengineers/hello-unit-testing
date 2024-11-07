a690181ba4be73326fd2d63ae52832065351f71d
Aleksandar Simeonov <aleksandar.simeonov@gmail.com>
2024-11-07 13:04:04 +0100

    solution 9: tdd - passing

--- components/voltage_monitor/src/voltage_monitor.c
+++ components/voltage_monitor/src/voltage_monitor.c
@@ -1,1 +1,55 @@
 #include "voltage_monitor.h"
+#include "voltage_adc.h"
+
+#define VOLTAGE_MAX 16000   /* Maximal measured voltage */
+#define ADC_MAX     0x3FFU  /* Maximal ADC value */
+#define V_LOW       10000U
+#define V_HIGH      14000U
+
+static uint16_t current_voltage = 0U;
+static VoltageStatus voltage_status = NOT_VALID;
+
+/* Voltage monitor initialization */
+void voltageMonitorInit(void)
+{
+    current_voltage = 0U;
+    voltage_status = NOT_VALID;
+}
+
+/* Get the voltage value and status */
+VoltageStatus getVoltage(uint16_t* voltage)
+{
+    *voltage = current_voltage;
+
+    return voltage_status;
+}
+
+/* Voltage converter cyclic function */
+void voltageMonitorCyclic(void)
+{
+    uint16_t adc_value = 0U;
+    ADCReturnType result = getADCValue(&adc_value);
+
+    if (result == ADC_READ_OK)
+    {
+        /*Convert ADC value */
+        current_voltage = (uint16_t)(((uint32_t)adc_value * VOLTAGE_MAX) / ADC_MAX);
+
+        if (current_voltage < V_LOW)
+        {
+            voltage_status = LOW_VOLTAGE;
+        }
+        else if (current_voltage > V_HIGH)
+        {
+            voltage_status = HIGH_VOLTAGE;
+        }
+        else
+        {
+            voltage_status = NORMAL_VOLTAGE;
+        }
+    }
+    else
+    {
+        voltage_status = NOT_VALID;
+    }
+}
--- components/voltage_monitor/src/voltage_monitor.h
+++ components/voltage_monitor/src/voltage_monitor.h
@@ -1,4 +1,23 @@
 #ifndef VOLTAGE_MONITOR
 #define VOLTAGE_MONITOR
 
+#include <stdint.h>
+
+typedef enum
+{
+    NOT_VALID,
+    LOW_VOLTAGE,
+    HIGH_VOLTAGE,
+    NORMAL_VOLTAGE
+} VoltageStatus;
+
+/* Voltage monitor initialization */
+extern void voltageMonitorInit(void);
+
+/* Get the voltage value and status */
+extern VoltageStatus getVoltage(uint16_t* voltage);
+
+/* Voltage monitor cyclic function */
+extern void voltageMonitorCyclic(void);
+
 #endif /* VOLTAGE_MONITOR */
