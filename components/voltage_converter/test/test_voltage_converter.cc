ae41b39f523200e3b5a993279e2d5e3c3413fceb
Aleksandar Simeonov <aleksandar.simeonov@gmail.com>
2024-11-07 08:49:32 +0100

    solution 8: voltage converter - passing

--- components/voltage_converter/test/test_voltage_converter.cc
+++ components/voltage_converter/test/test_voltage_converter.cc
@@ -6,7 +6,22 @@ extern "C"
 #include "voltage_converter.h"
 }
 
-TEST(VoltageConverterTest, FailingTest)
+#include "mockup_components_voltage_converter.h"
+
+#define MAX_ADC         0xFFFU
+#define MAX_VOLTAGE     16.0f
+#define ABS_ERROR       4e-3f   // One step is 16.0/0xFFFU = 0.0039...
+
+class VoltageConverterTest : public Test
+{
+protected:
+    void SetUp(void) override
+    {
+        voltageConverterInit();
+    }
+};
+
+TEST_F(VoltageConverterTest, Initialization)
 {
     /* Arrange */
 
@@ -14,5 +29,62 @@ TEST(VoltageConverterTest, FailingTest)
     float voltage = getVoltage();
 
     /* Assert */
-    EXPECT_TRUE(false);
+    EXPECT_NEAR(voltage, 0.0f, ABS_ERROR);
+}
+
+TEST_F(VoltageConverterTest, Zero)
+{
+    /* Arrange */
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, getADCValue(_)).Times(1).WillOnce(DoAll(SetArgPointee<0>(0U), Return(ADC_READ_OK)));
+
+    /* Act */
+    voltageConverterCyclic();
+
+    /* Assert */
+    float voltage = getVoltage();
+    EXPECT_NEAR(voltage, 0.0f, ABS_ERROR);
+}
+
+TEST_F(VoltageConverterTest, FullRange)
+{
+    /* Arrange */
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, getADCValue(_)).Times(1).WillOnce(DoAll(SetArgPointee<0>(MAX_ADC), Return(ADC_READ_OK)));
+
+    /* Act */
+    voltageConverterCyclic();
+
+    /* Assert */
+    float voltage = getVoltage();
+    EXPECT_NEAR(voltage, MAX_VOLTAGE, ABS_ERROR);
+}
+
+TEST_F(VoltageConverterTest, HalfRange)
+{
+    /* Arrange */
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, getADCValue(_)).Times(1).WillOnce(DoAll(SetArgPointee<0>(MAX_ADC / 2), Return(ADC_READ_OK)));
+
+    /* Act */
+    voltageConverterCyclic();
+
+    /* Assert */
+    float voltage = getVoltage();
+    EXPECT_NEAR(voltage, MAX_VOLTAGE / 2.0f, ABS_ERROR);
+}
+
+TEST_F(VoltageConverterTest, ADCReadingError)
+{
+    /* Arrange */
+    CREATE_MOCK(myMock);
+    EXPECT_CALL(myMock, getADCValue(_)).Times(2).WillOnce(DoAll(SetArgPointee<0>(MAX_ADC), Return(ADC_READ_OK))).WillOnce(DoAll(SetArgPointee<0>(MAX_ADC / 2), Return(ADC_READ_NOK)));
+
+    /* Act */
+    voltageConverterCyclic();
+    voltageConverterCyclic();
+
+    /* Assert */
+    float voltage = getVoltage();
+    EXPECT_NEAR(voltage, MAX_VOLTAGE, ABS_ERROR);
 }
